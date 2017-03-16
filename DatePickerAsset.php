6a8e35005d039365296f418b735d14ec72698962
Newby <Newby_30603@hotmail.com>
2017-03-16 17:49:49 +0700

    change to use datepicker th for buddhist

--- DatePickerAsset.php
+++ DatePickerAsset.php
@@ -26,7 +26,7 @@ class DatePickerAsset extends AssetBundle
     {
         $this->setSourcePath(__DIR__ . '/assets');
         $this->setupAssets('css', ['css/bootstrap-datepicker3', 'css/datepicker-kv']);
-        $this->setupAssets('js', ['js/bootstrap-datepicker', 'js/datepicker-kv','js/bootstrap-datepicker-thai']);
+        $this->setupAssets('js', ['js/bootstrap-datepicker-th', 'js/datepicker-kv']);
         parent::init();
     }
 }
